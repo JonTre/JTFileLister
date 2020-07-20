fd8b23f733367ec9a2fe4aacecf2035c9bf67190
JonTre <65495046+JonTre@users.noreply.github.com>
2020-07-20 11:09:29 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace JTFileLister
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dirCountLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fileCountLbl = new System.Windows.Forms.Label();
+            this.currFileLbl = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.currDirLbl = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(12, 130);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopButton.Location = new System.Drawing.Point(188, 129);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Directory Count:";
+            // 
+            // dirCountLbl
+            // 
+            this.dirCountLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dirCountLbl.AutoEllipsis = true;
+            this.dirCountLbl.Location = new System.Drawing.Point(126, 16);
+            this.dirCountLbl.Name = "dirCountLbl";
+            this.dirCountLbl.Size = new System.Drawing.Size(136, 15);
+            this.dirCountLbl.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "File Count:";
+            // 
+            // fileCountLbl
+            // 
+            this.fileCountLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileCountLbl.AutoEllipsis = true;
+            this.fileCountLbl.Location = new System.Drawing.Point(126, 43);
+            this.fileCountLbl.Name = "fileCountLbl";
+            this.fileCountLbl.Size = new System.Drawing.Size(136, 15);
+            this.fileCountLbl.TabIndex = 3;
+            // 
+            // currFileLbl
+            // 
+            this.currFileLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currFileLbl.AutoEllipsis = true;
+            this.currFileLbl.Location = new System.Drawing.Point(126, 97);
+            this.currFileLbl.Name = "currFileLbl";
+            this.currFileLbl.Size = new System.Drawing.Size(136, 15);
+            this.currFileLbl.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 15);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Current File:";
+            // 
+            // currDirLbl
+            // 
+            this.currDirLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currDirLbl.AutoEllipsis = true;
+            this.currDirLbl.Location = new System.Drawing.Point(126, 70);
+            this.currDirLbl.Name = "currDirLbl";
+            this.currDirLbl.Size = new System.Drawing.Size(136, 15);
+            this.currDirLbl.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 15);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Current Directory:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(6, 165);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(256, 64);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 241);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.currDirLbl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.currFileLbl);
+            this.Controls.Add(this.fileCountLbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dirCountLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.goButton);
+            this.MinimumSize = new System.Drawing.Size(290, 280);
+            this.Name = "Form1";
+            this.Text = "Interview Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label dirCountLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label fileCountLbl;
+        private System.Windows.Forms.Label currFileLbl;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label currDirLbl;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace JTFileLister
+{
+    //Jonathan Tregoiing
+    //cv@Tregoiing.co.uk
+    //074720 78720
+
+    //requestedExecutionLevel = "requireAdministrator"
+    //This has been requested in the app.manifest file
+    public partial class Form1 : Form
+    {
+        //Our JSON object - Note, this can output CSV too.
+        public class FileInfoForOutput
+        {
+            public string Name { get; set; }
+            public string DirectoryName { get; set; }
+            public DateTimeOffset CreationTimeUtc { get; set; }
+            public DateTimeOffset LastWriteTimeUtc { get; set; }
+        }
+
+        //Global BW to allow the user to still control the app
+        private BackgroundWorker backgroundWorker1 = new BackgroundWorker
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true
+        };
+
+        //Some setup and constants - (Localisation not supported in V1.0)
+        private int directoryCount = 0;
+        private int fileCount = 0;
+        private const string EarlyShutdown = "Please use the 'Stop' button when the scan is in progress";
+        private const string ProblemClosingLogFile = "Problem encountered closing the log file";
+        //WantJSON = false will produce a CSV format with header and footer row
+        private const bool WantJSON = true; 
+
+        //Our starting point - Ideally needs to be user selectable at runtime
+        private string root = @"C:\";
+        private FileStream logZipFile;
+        private ZipArchive logZipFileArchive;
+        private ZipArchiveEntry readmeEntry;
+        private StreamWriter logStream;
+        private JsonSerializerOptions options;
+
+        private void InitAll()
+        {
+            directoryCount = 0;
+            fileCount = 0;
+            //Go and Stop buttons are only enabled if they make sense in the current state
+            stopButton.Enabled = true;
+            textBox1.Clear();
+            WriteToStatusLog("Start");
+
+            var path = @".\";
+            var logFileName = @"Log";
+            var logCounter = 1;
+            var fileName = DateTime.Now.ToString("yyyyMMdd") + ".zip";
+            //Logging to a zip archive
+            logZipFile = new FileStream(path + fileName, FileMode.OpenOrCreate);
+            {
+                logZipFileArchive = new ZipArchive(logZipFile, ZipArchiveMode.Update);
+                {
+                    while (logZipFileArchive.GetEntry(logFileName + logCounter.ToString() + ".txt") != null)
+                    {
+                        logCounter++;
+                    }
+                    readmeEntry = logZipFileArchive.CreateEntry($"{logFileName}{ logCounter}.txt");
+                    logStream = new StreamWriter(readmeEntry.Open());
+                }
+            }
+            if (WantJSON)
+                WriteToLog(@"{""Files"":[");
+            else
+                WriteToLog("Name,DirectoryName,CreationTimeUtc,LastWriteTimeUtc");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            stopButton.Enabled = false;
+            options = new JsonSerializerOptions
+            {
+                IgnoreNullValues = true,
+                WriteIndented = true
+            };
+
+            //Set the events
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                //Let's start!
+                InitAll();
+                goButton.Enabled = false;
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //Early termination requested
+            stopButton.Enabled = false;
+            if (backgroundWorker1.WorkerSupportsCancellation)
+                backgroundWorker1.CancelAsync();
+        }
+
+        private void addTologFromThread(string logMessage)
+        {
+            //Threadsafe UI conduit 
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                WriteToLog(logMessage);
+            });
+        }
+        private void addToStatusLogFromThread(string logMessage)
+        {
+            //Threadsafe UI conduit 
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                WriteToStatusLog(logMessage);
+            });
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs args)
+        {
+            //The main event!
+            //Scan through everything that we've got access to.
+            //Running as a Windows Service would allow us to gave greater access
+            //Check for errors as we go - log exceptions onto the screen - file info goes into the archive file
+            var worker = sender as BackgroundWorker;
+            //We're going to use a stack for this - This should allow better resource management for the potentially large number of folders
+            var dirs = new Stack<string>(20);
+
+            if (!System.IO.Directory.Exists(root))
+                throw new ArgumentException();
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    //Request update to the current directory label
+                    UpdateDirectoryInformation(currentDir);
+                });
+                try
+                {
+                    subDirs = System.IO.Directory.GetDirectories(currentDir);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    //An error...
+                    addToStatusLogFromThread(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    addToStatusLogFromThread(e.Message);
+                    continue;
+                }
+                string[] files = null;
+                try
+                {
+                    files = System.IO.Directory.GetFiles(currentDir);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    addToStatusLogFromThread(e.Message);
+                    continue;
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    addToStatusLogFromThread(e.Message);
+                    continue;
+                }
+
+                foreach (string file in files)
+                {
+                    //Was a 'Stop' requested?
+                    if (backgroundWorker1.CancellationPending)
+                    {
+                        args.Cancel = true;
+                        dirs.Clear();
+                        break;
+                    }
+                    try
+                    {
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+
+                            System.IO.FileInfo fi = new System.IO.FileInfo(file);
+                            //Request update to the current file label
+                            UpdateFileInformation(fi.Name);
+
+                            //Setup JSON object
+                            var fileInfoForOutput = new FileInfoForOutput
+                            {
+                                Name = fi.Name,
+                                DirectoryName = fi.DirectoryName,
+                                CreationTimeUtc = fi.CreationTimeUtc,
+                                LastWriteTimeUtc = fi.LastWriteTimeUtc
+                            };
+                            if (WantJSON)
+                            {
+                                //Dump out JSON object
+                                if (fileCount > 1)
+                                    addTologFromThread(",");
+                                addTologFromThread(JsonSerializer.Serialize(fileInfoForOutput, options));
+                            }
+                            else
+                            {
+                                //Dump out CSV line
+                                addTologFromThread(String.Format("{0},{1},{2},{3}", fi.Name, fi.DirectoryName, fi.CreationTimeUtc, fi.LastWriteTimeUtc));
+                            }
+                        });
+                    }
+                    catch (System.IO.FileNotFoundException e)
+                    {
+                        addToStatusLogFromThread(e.Message);
+                        continue;
+                    }
+                }
+
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                WriteToStatusLog("Canceled!");
+            else if (e.Error != null)
+                WriteToStatusLog("Error: " + e.Error.Message);
+            else
+            {
+                WriteToStatusLog("Done!");
+                if (!WantJSON)
+                    WriteToLog("End of data");
+            }
+            //Properly end the JSON file
+            if (WantJSON)
+                WriteToLog("]}");
+            CloseFile();
+            goButton.Enabled = true;
+        }
+        private void UpdateDirectoryInformation(string directoryName)
+        {
+            directoryCount++;
+            currDirLbl.Text = directoryName;
+            dirCountLbl.Text = directoryCount.ToString();
+        }
+        private void UpdateFileInformation(string fileName)
+        {
+            fileCount++;
+            currFileLbl.Text = fileName;
+            fileCountLbl.Text = fileCount.ToString();
+        }
+
+        private void WriteToLog(string textToLog)
+        {
+            if (logStream != null)
+                logStream.WriteLine(textToLog);
+        }
+        private void WriteToStatusLog(string textToLog)
+        {
+            //Update the on screen textbox
+            textBox1.AppendText(textToLog + Environment.NewLine);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Click Stop then close the app didn't happen - remind the user!
+            if (backgroundWorker1.IsBusy)
+            {
+                stopButton_Click(sender, e);
+                e.Cancel = true;
+                MessageBox.Show(EarlyShutdown, this.Text, MessageBoxButtons.OK);
+            }
+            CloseFile();
+        }
+
+        private void CloseFile()
+        {
+            try
+            {
+                //Shutdown the file 
+                if (logStream != null)
+                {
+                    logStream.Close();
+                    logStream.Dispose();
+                    logZipFileArchive.Dispose();
+                    logZipFile.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ProblemClosingLogFile + Environment.NewLine + ex.InnerException.Message, this.Text, MessageBoxButtons.OK);
+            }
+        }
+    }
+}
